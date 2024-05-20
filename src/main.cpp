6298baa425bc0bd8c6979f571bf57cffbe2739d3
Julio <juliomoreno7217@gmail.com>
2024-05-20 17:37:37 -0600

    Doc: Autonomous Car project created
    The first library was imported: "Servo" and definitions of the functions and control of a single Servomotor works fine

---
+++ src/main.cpp
@@ -0,0 +1,57 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+
+//////GPIO PIN DEFINITION//////
+//////GPIO PIN DEFINITION//////
+
+// Servo 
+const int SERVOPIN = 33;    //Pin 8
+Servo servoMotor;
+
+//////INIT FUNCTIONS//////
+//////INIT FUNCTIONS//////
+
+//Servo
+void servoInit(){
+  Serial.println("Servomotor initiated");
+  servoMotor.attach(SERVOPIN);
+
+}
+
+//////BEHAVIORAL FUNCTIONS//////
+//////BEHAVIORAL FUNCTIONS//////
+
+//Servo
+void setServoRight(){
+  //for debugging
+  Serial.println("ServoRight");
+
+  servoMotor.write(20);
+}
+void setServoLeft(){
+  //for debugging
+  Serial.println("ServoLeft");
+  servoMotor.write(135);
+}
+void setServoMiddle(){
+  //for debugging
+  Serial.println("ServoMiddle");
+  servoMotor.write(80);
+}
+
+
+void setup() {
+  servoInit();
+
+}
+
+void loop() {
+
+  setServoLeft();
+  delay(300);
+  setServoMiddle();
+  delay(300);
+  setServoRight();
+  delay(300);
+
+}
