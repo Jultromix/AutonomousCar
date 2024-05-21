e2988ed231ee2231534634100f35948d3ca71be0
Julio <juliomoreno7217@gmail.com>
2024-05-20 18:08:14 -0600

    Feat: DC Motor contol functionality added

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,13 @@
 //////GPIO PIN DEFINITION//////
 
 // Servo 
-const int SERVOPIN = 32;    //Pin 8
+const int SERVOPIN = 32;    //Pin 7
 Servo servoMotor;
+
+  // DC motor
+const int ENA = 33;    // Pin 8 
+const int IN1 = 26;    // Pin 9
+const int IN2 = 25;    // Pin 10
 
 //////INIT FUNCTIONS//////
 //////INIT FUNCTIONS//////
@@ -15,7 +20,17 @@ Servo servoMotor;
 void servoInit(){
   Serial.println("Servomotor initiated");
   servoMotor.attach(SERVOPIN);
+}
 
+//DC Motor
+void dcMotorInit(){
+  Serial.println("DC Motor initiated");
+  pinMode (ENA, OUTPUT); 
+  pinMode (IN1, OUTPUT);
+  pinMode (IN2, OUTPUT);
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, LOW);
+  analogWrite(ENA,0);
 }
 
 //////BEHAVIORAL FUNCTIONS//////
@@ -38,10 +53,31 @@ void setServoMiddle(){
   servoMotor.write(80);
 }
 
+//DC Motor
+void setMotorForward(){
+  Serial.println("DCMotorForward");
+  digitalWrite (IN1, HIGH);
+  digitalWrite (IN2, LOW);
+  analogWrite(ENA,200);
+}
+void setMotorBackward(){
+  Serial.println("DCMotorBackward");
+  digitalWrite (IN2, HIGH);
+  digitalWrite (IN1, LOW);
+  analogWrite(ENA,200);
+}
+void setMotorStop(){
+  Serial.println("DCMotorStop");
+  digitalWrite (IN1, LOW);
+  digitalWrite (IN2, LOW);
+  analogWrite(ENA,0);
+}
+
 
 void setup() {
   Serial.begin(115200);
   servoInit();
+  dcMotorInit();
 
 }
 
